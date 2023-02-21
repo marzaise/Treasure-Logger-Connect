60915ac831502938b50beb1100ded023d7b9af4a
Marzaise <>
2023-02-21 19:58:21 +0100

    improving demo

--- epd2in7_V2-demo/epd2in7_V2-demo.ino
+++ epd2in7_V2-demo/epd2in7_V2-demo.ino
@@ -12,54 +12,101 @@ void setup()
     DEV_Module_Init();
 
     printf("e-Paper Init and Clear...\r\n");
-    EPD_2IN7_V2_Init();
+    EPD_2IN7_V2_Init_4GRAY();
     EPD_2IN7_V2_Clear();
     delay(500);
 
     //Create a new image cache
     UBYTE *BlackImage;
     /* you have to edit the startup_stm32fxxx.s file and set a big enough heap size */
-    UWORD Imagesize = ((EPD_2IN7_V2_WIDTH % 8 == 0)? (EPD_2IN7_V2_WIDTH / 8 ): (EPD_2IN7_V2_WIDTH / 8 + 1)) * EPD_2IN7_V2_HEIGHT;
-    if ((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
-        printf("Failed to apply for black memory...\r\n");
-        while (1);
-    }
-    printf("Paint_NewImage\r\n");
-    Paint_NewImage(BlackImage, EPD_2IN7_V2_WIDTH, EPD_2IN7_V2_HEIGHT, 270, WHITE);
 
 
+
+    /*
     free(BlackImage);
     printf("show Gray------------------------\r\n");
-    Imagesize = ((EPD_2IN7_V2_WIDTH % 4 == 0)? (EPD_2IN7_V2_WIDTH / 4 ): (EPD_2IN7_V2_WIDTH / 4 + 1)) * EPD_2IN7_V2_HEIGHT;
+    UWORD Imagesize = ((EPD_2IN7_V2_WIDTH % 4 == 0)? (EPD_2IN7_V2_WIDTH / 4 ): (EPD_2IN7_V2_WIDTH / 4 + 1)) * EPD_2IN7_V2_HEIGHT;
     if((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
         printf("Failed to apply for black memory...\r\n");
         while (1);
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#if 1   // show image for array   
+   // show image for array   
     printf("show image for array\r\n");
     Paint_SelectImage(BlackImage);
     Paint_Clear(WHITE);
-    Paint_DrawBitMap(gImage_2in7);
-    EPD_2IN7_V2_Display(BlackImage);
+    Paint_DrawBitMap(gImage_2in7_4Gray);
+    EPD_2IN7_V2_4GrayDisplay(BlackImage);
     delay(500);
-#endif
+
+    */
+
+
+
+
+
+
+
+
+
+    free(BlackImage);
+    printf("show Gray------------------------\r\n");
+    UWORD Imagesize = ((EPD_2IN7_V2_WIDTH % 4 == 0)? (EPD_2IN7_V2_WIDTH / 4 ): (EPD_2IN7_V2_WIDTH / 4 + 1)) * EPD_2IN7_V2_HEIGHT;
+    if((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
+        printf("Failed to apply for black memory...\r\n");
+        while (1);
+    }
+    EPD_2IN7_V2_Init_4GRAY();
+    printf("4 grayscale display\r\n");
+    Paint_NewImage(BlackImage, EPD_2IN7_V2_WIDTH, EPD_2IN7_V2_HEIGHT, 90, WHITE);
+    Paint_SetScale(4);
+    Paint_Clear(0xff);
+    
+
+    /*
+    Paint_DrawPoint(10, 80, GRAY4, DOT_PIXEL_1X1, DOT_STYLE_DFT);
+    Paint_DrawPoint(10, 90, GRAY4, DOT_PIXEL_2X2, DOT_STYLE_DFT);
+    Paint_DrawPoint(10, 100, GRAY4, DOT_PIXEL_3X3, DOT_STYLE_DFT);
+    Paint_DrawLine(20, 70, 70, 120, GRAY4, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
+    Paint_DrawLine(70, 70, 20, 120, GRAY4, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
+    Paint_DrawRectangle(20, 70, 70, 120, GRAY4, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
+    Paint_DrawRectangle(80, 70, 130, 120, GRAY4, DOT_PIXEL_1X1, DRAW_FILL_FULL);
+    Paint_DrawCircle(45, 95, 20, GRAY4, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
+    Paint_DrawCircle(105, 95, 20, GRAY2, DOT_PIXEL_1X1, DRAW_FILL_FULL);
+    Paint_DrawLine(85, 95, 125, 95, GRAY4, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
+    Paint_DrawLine(105, 75, 105, 115, GRAY4, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
+    Paint_DrawString_EN(10, 0, "waveshare", &Font16, GRAY4, GRAY1);
+    Paint_DrawString_EN(10, 20, "hello world", &Font12, GRAY3, GRAY1);
+    Paint_DrawNum(10, 33, 123456789, &Font12, GRAY4, GRAY2);
+    Paint_DrawNum(10, 50, 987654321, &Font16, GRAY1, GRAY4);
+    Paint_DrawString_CN(150, 0,"你好abc", &Font12CN, GRAY4, GRAY1);
+    Paint_DrawString_CN(150, 20,"你好abc", &Font12CN, GRAY3, GRAY2);
+    Paint_DrawString_CN(150, 40,"你好abc", &Font12CN, GRAY2, GRAY3);
+    Paint_DrawString_CN(150, 60,"你好abc", &Font12CN, GRAY1, GRAY4);
+    Paint_DrawString_CN(10, 130, "微雪电子", &Font24CN, GRAY1, GRAY4);
+    EPD_2IN7_V2_4GrayDisplay(BlackImage);
+    */
+
+    unsigned char* arr = (unsigned char *) malloc(sizeof(unsigned char) * 11616);
+    for (int i=0; i<2904; ++i)
+        arr[i] = 0b00000000; //black
+
+    for (int i=2904; i<2904*2; ++i)
+        arr[i] = 0b01010101; //dark gray
+
+    for (int i=2904*2; i<2904*3; ++i)
+      arr[i] = 0b10101010; //light gray
+
+    for (int i=2904*3; i<2904*4; ++i)
+      arr[i] = 0b11111111; //white
+    
+    Paint_DrawBitMap(arr);
+    EPD_2IN7_V2_4GrayDisplay(BlackImage);
+
+
+
+/*
 
 #if 1  // Drawing on the image
     Paint_NewImage(BlackImage, EPD_2IN7_V2_WIDTH, EPD_2IN7_V2_HEIGHT, 90, WHITE);    
@@ -255,6 +302,7 @@ void setup()
     // close 5V
     printf("close 5V, Module enters 0 power consumption ...\r\n");
 
+*/
 }
 
 /* The main loop -------------------------------------------------------------*/
